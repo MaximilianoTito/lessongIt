053f9a31e1f80d6f7115fa35e778fc23a36211d6
Max <mmtito@yavirac.edu.ec>
2024-08-16 23:57:08 -0500

    guardado

--- prueba.py
+++ prueba.py
@@ -1,5 +1,7 @@
 ## creacion de variable
+nombre = 'Te amo Alejandra'
 x = 0
 ## productos de primera necesidad
 product = ["manzana", "pera", "aguacate", "naranja", "fideo"]
 print(product[2])
+print(nombre)
